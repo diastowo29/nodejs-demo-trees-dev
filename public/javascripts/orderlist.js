fdfe6666817b9c5cf50b8c9541edad8de2b1b483
Diastowo <diastowo@gmail.com>
2020-07-16 21:51:01 +0700

    fixing div by id

--- public/javascripts/orderlist.js
+++ public/javascripts/orderlist.js
@@ -1,11 +1,12 @@
 function takeOrder (id) {
+	var divToHide = $('div#' + id + '')
 	$.ajax({
         url: '/arduino/kantin/order/take?id=' + id,
         type: 'get',
         dataType: 'json',
         contentType: 'application/json',
         success: function (data) {
-			$('div[id^=' + id + ']').hide();
+        	divToHide.hide();
         },
     });
 }
