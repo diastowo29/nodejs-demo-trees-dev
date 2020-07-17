e92609988541c190d35aa9396277241e07585d9c
TreesZendesk <diastowo@gmail.com>
2020-07-17 10:21:42 +0700

    debugging heroku

--- public/javascripts/orderlist.js
+++ public/javascripts/orderlist.js
@@ -1,12 +1,15 @@
 function takeOrder (id) {
+    console.log('takeOrder')
+    console.log(id)
 	var divToHide = $('div#' + id + '')
-	$.ajax({
-        url: '/arduino/kantin/order/take?id=' + id,
-        type: 'get',
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (data) {
-        	divToHide.hide();
-        },
-    });
+    console.log(divToHide)
+	// $.ajax({
+ //        url: '/arduino/kantin/order/take?id=' + id,
+ //        type: 'get',
+ //        dataType: 'json',
+ //        contentType: 'application/json',
+ //        success: function (data) {
+ //        	divToHide.hide();
+ //        },
+ //    });
 }
