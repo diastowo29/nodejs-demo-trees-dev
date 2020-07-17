325262175c6d21a0bf537dec916414301b68ba84
TreesZendesk <diastowo@gmail.com>
2020-07-17 10:33:04 +0700

    fixed

--- public/javascripts/orderlist.js
+++ public/javascripts/orderlist.js
@@ -1,16 +1,13 @@
 function takeOrder (btnOrder) {
-    console.log('takeOrder')
     let btnId = $(btnOrder).attr('id')
-    console.log(btnId)
 	var divToHide = $('div#' + btnId + '')
-    console.log(divToHide)
-	// $.ajax({
- //        url: '/arduino/kantin/order/take?id=' + id,
- //        type: 'get',
- //        dataType: 'json',
- //        contentType: 'application/json',
- //        success: function (data) {
- //        	divToHide.hide();
- //        },
- //    });
+	$.ajax({
+        url: '/arduino/kantin/order/take?id=' + id,
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+        	divToHide.hide();
+        },
+    });
 }
