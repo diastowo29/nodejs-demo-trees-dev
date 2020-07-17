9b7c9508e6e2f8f0260dfd4df43737abf588e582
TreesZendesk <diastowo@gmail.com>
2020-07-17 10:36:55 +0700

    ketinggalan

--- public/javascripts/orderlist.js
+++ public/javascripts/orderlist.js
@@ -2,7 +2,7 @@ function takeOrder (btnOrder) {
     let btnId = $(btnOrder).attr('id')
 	var divToHide = $('div#' + btnId + '')
 	$.ajax({
-        url: '/arduino/kantin/order/take?id=' + id,
+        url: '/arduino/kantin/order/take?id=' + btnId,
         type: 'get',
         dataType: 'json',
         contentType: 'application/json',
